7f3157f8450282130924b82b430578e419977574
Vo Minh Dat <vo_minhdat2007@yahoo.com.vn>
2012-05-08 14:19:26 +0700

    Initialize C# Project with Matrix class.

---
+++ Matrix/Matrix/Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+
+    /// <summary>
+    /// A instance of a matrix and perform operators on matrices.
+    /// <remarks>
+    ///     Author: DatVM
+    ///     Version: 1.0
+    ///     Since: 2012-May-08
+    /// </remarks>
+    /// </summary>
+    public class Matrix
+    {
+
+        //===============================================================================//
+        // Constructors
+        //===============================================================================//
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Matrix()
+        {
+            
+        }
+
+        //===============================================================================//
+        // Methods
+        //===============================================================================//
+
+
+
+    }
+
+}
